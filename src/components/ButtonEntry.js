c6d5471f21f334aed45dc5fa0f8a68413219b231
Valeriia Braterska <vsirenina@gmail.com>
2017-02-11 13:19:32 +0200

    Update ButtonEntry.js

--- src/components/ButtonEntry.js
+++ src/components/ButtonEntry.js
@@ -9,15 +9,15 @@ class ButtonEntry extends Component {
           modalOpened: false
         };
         this.modalToggle = this.modalToggle.bind(this);
+        this.handleAddClick = this.handleAddClick.bind(this);
     }
 
     modalToggle () {
       this.setState({ modalOpened: !this.state.modalOpened });
     }
 
-    onAddBtnClick(event){
-        addAuto(event.target.value)
-     //   console.log(addAuto(event.target.value))
+    handleAddClick(event){
+        this.props.onAddBtnClick(event)
     }
     render() {
         const containerClass = this.state.modalOpened ? 'modal-container modal-container-active' : 'modal-container';
@@ -28,9 +28,9 @@ class ButtonEntry extends Component {
                     <div className='modal-body select_auto'>
                         <h3>Choose type auto:</h3>
                         <div className='select_auto_container'>
-                            <button className='change_auto sedan_select' value='sedan' onClick={this.onAddBtnClick}>sedan</button>
-                            <button className='change_auto disabled_select' value='disabled' onClick={this.onAddBtnClick}>disabled</button>
-                            <button className='change_auto truck_select' value='truck' onClick={this.onAddBtnClick}>truck</button>
+                            <button className='change_auto sedan_select' value='sedan' onClick={this.handleAddClick}>sedan</button>
+                            <button className='change_auto disabled_select' value='disabled' onClick={this.handleAddClick}>disabled</button>
+                            <button className='change_auto truck_select' value='truck' onClick={this.handleAddClick}>truck</button>
                         </div>
                     </div>
                 </div>
@@ -41,7 +41,7 @@ class ButtonEntry extends Component {
 
 const mapDispatchToProps = (dispatch) =>
     ({
-        onAddBtnClick: (type_auto) => dispatch(addAuto(type_auto))
+        onAddBtnClick: (event) => dispatch(addAuto(event.target.value))
     })
 
-export default connect(mapDispatchToProps)(ButtonEntry);
+export default connect(null, mapDispatchToProps)(ButtonEntry);
